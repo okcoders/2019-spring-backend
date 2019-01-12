13e8d20c0a707d3a15bfedb2680d55d49c5f20ed
Bob Rockers <bob@rockerssoft.org>
2019-01-12 08:25:01 -0600

    Lots of updates, soon we will have world domination.

--- 02-express/challenges/app.js
+++ 02-express/challenges/app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+var port = 3000;
 
 app.get('/hello', function(req,res){
 	res.send("<html><h1>Hello World</h1></html>");
 });
 
-app.listen('3000', function(){
+app.put('/goodbye', function(req,res){
+	res.send("<html><h1>Goodbye</h1></html>");
+});
+
+app.listen('8000', function(){
 	console.log("Express running on port 3000");
 });
