6fa4786951da5228eede71378c98658c6e0ee836
Bob Rockers <bob@rockerssoft.org>
2019-01-12 08:34:09 -0600

    Challenge questions.

--- 02-express/challenges/app.js
+++ 02-express/challenges/app.js
@@ -3,11 +3,19 @@ var app = express();
 var port = 3000;
 
 app.get('/hello', function(req,res){
-	res.send("<html><h1>Hello World</h1></html>");
+	res.send("<html><h1>Goodbye World</h1></html>");
 });
 
 app.put('/goodbye', function(req,res){
 	res.send("<html><h1>Goodbye</h1></html>");
+});
+
+app.get('/messages/*', function(req,res){
+	res.send("You have 10 messages");
+});
+
+app.get('/messages/new', function(req,res){
+	res.send("You have 5 new messages");
 });
 
 app.listen('8000', function(){
